e484083de879a14671bbcff8937808f139662cd5
Byron Bustamante <byronbl.12@gmail.com>
2019-08-25 18:32:51 +0100

    Terminada la parte del layout

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,11 +1,11 @@
 const router = require('express').Router() // Para crear rutas
 
 router.get('/users/signin', (req, res) => {
-	res.send('Ingresando a la app')
+	res.render('users/signin')
 })
 
 router.get('/users/signup', (req, res) => {
-	res.send('Formulario de autenticacion')
+	res.render('users/signup')
 })
 
 module.exports = router; // Exportamos el router
