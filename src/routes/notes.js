1f137b0a4de731f1c7d19a5a0a0f12d4ec7f4b2e
Byron Bustamante <byronbl.12@gmail.com>
2019-08-25 19:59:26 +0100

    Terminada la funcionalidad de crear notas y listarlas

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,7 +1,39 @@
 const router = require('express').Router() // Para crear rutas
 
-router.get('/notes', (req, res) => {
-	res.send('Notes from database')
+const Note = require('../models/Note')
+
+router.get('/notes/add', (req, res) => {
+	res.render('notes/new-note')
+})
+
+router.post('/notes/new-note', async (req, res) => {
+	const { title, description } = req.body // Obtenemos el titulo y la descripcion del formulario enviado
+
+	// Validacion de datos
+	const errors = []
+	if(!title)
+		errors.push({text: 'Please, write a title'})
+
+	if(!description)
+		errors.push({text: 'Please, write a description'})
+	
+	if(errors.length > 0)
+		res.render('notes/new-note', {
+			errors,
+			title,
+			description
+		})
+	// Si todo va bien
+	else {
+		const newNote = new Note({ title, description }) // Crea un objeto Note
+		await newNote.save(); // Guarda los datos
+		res.redirect('/notes') // Redireccionamos para ver todas las notas
+	}
+})
+
+router.get('/notes', async (req, res) => {
+	const notes = await	Note.find().sort({date: 'desc'}) // Obtenemos todas las notas
+	res.render('notes/all-notes', { notes }) // Renderizamos la vista con todas las notas
 })
 
 module.exports = router; // Exportamos el router
